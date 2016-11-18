d2945693959fc4ff85c478160cf618d2fcfa62b5
爱任信 <153751320@qq.com>
2016-11-18 14:33:02 +0800

    Initial Commit

---
+++ Masonry测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masonry测试
+//
+//  Created by 爱任信 on 16/11/18.
+//  Copyright © 2016年 爱任信. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Masonry测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Masonry测试
+//
+//  Created by 爱任信 on 16/11/18.
+//  Copyright © 2016年 爱任信. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
